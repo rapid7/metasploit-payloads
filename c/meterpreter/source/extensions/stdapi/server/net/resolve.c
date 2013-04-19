b96062380cc1cf5c44046672978e2642e9d298da
Meatballs <eat_meatballs@hotmail.co.uk>
2013-04-19 11:06:52 +0100

    Use getaddrinfo

--- c/meterpreter/source/extensions/stdapi/server/net/resolve.c
+++ c/meterpreter/source/extensions/stdapi/server/net/resolve.c
@@ -2,68 +2,89 @@
 #include <stdio.h>
 
 #ifdef _WIN32
-  #include <ws2tcpip.h>
   #include <winsock2.h>
+  #include <ws2tcpip.h>
 #else
+  #include <sys/types.h>
+  #include <sys/socket.h>
   #include <netdb.h>
-  #include <arpa/inet.h>
 #endif
 
-DWORD resolve_host(LPCSTR hostname, char* addr, u_short* addr_type, u_short* addr_length)
+DWORD resolve_host(LPCSTR hostname, u_short ai_family, struct in_addr *result)
 {
-	struct hostent *he;
-	int i = 0;
+	struct addrinfo hints;
+	struct addrinfo *list = NULL;
+	struct in_addr addr;
+	struct sockaddr_in *sockaddr_ipv4;
+	struct sockaddr_in6 *sockaddr_ipv6;
+	int iResult;
 
 #ifdef _WIN32
-	WSADATA data;
-	if (WSAStartup (MAKEWORD(1, 1), &data) != 0)
+	WSADATA wsaData;
+	iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+	if (iResult != NO_ERROR)
 	{
-		dprintf("Could not initialise Winsock.\n", stderr);
-		return 1;
+		dprintf("RESOLVE, resolve_host - Could not initialise Winsock: %x.\n", iResult);
+		return iResult;
 	}
 #endif
 
-	he = gethostbyname (hostname);
-	if (he == NULL)
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_family = ai_family;
+
+	iResult = getaddrinfo(hostname, NULL, &hints, &list);
+
+	if (iResult != NO_ERROR)
 	{
-		return h_errno;
+		dprintf("RESOLVE, resolve_host - Unable to resolve host %x.\n", iResult);
+	}
+	else
+	{
+		switch (list->ai_family) {
+		case AF_INET:
+			sockaddr_ipv4 = (struct sockaddr_in *) list->ai_addr;
+			addr = sockaddr_ipv4->sin_addr;
+			memcpy((void*)result, &addr, sizeof(addr));
+		case AF_INET6:
+			//todo
+		default:
+			break;
+		}
 	}
 
-	for(i = 0; i < he->h_length; i++)
-	{
-		memcpy(addr+i, &(he->h_addr_list[0][i]), 1);
-	}
-	*addr_type = he->h_addrtype;
-	*addr_length = he->h_length;
+	freeaddrinfo(list);
 
 #ifdef _WIN32
-	WSACleanup ();
+	WSACleanup();
 #endif
 
-	return h_errno;
+	return iResult;
 }
 
 DWORD request_resolve_host(Remote *remote, Packet *packet)
 {
 	Packet *response = packet_create_response(packet);
 	LPCSTR hostname = NULL;
-	char addr[16];
-	u_short addr_type;
-	u_short addr_length;
-	DWORD result = NULL;
+	struct in_addr raw;
+	u_short ai_family = AF_INET;
+	int iResult;
 
 	hostname = packet_get_tlv_value_string(packet, TLV_TYPE_HOST_NAME);
 
 	if (!hostname)
-		result = ERROR_INVALID_PARAMETER;
+		iResult = ERROR_INVALID_PARAMETER;
 	else
 	{
-		result = resolve_host(hostname, addr, &addr_type, &addr_length);
-		packet_add_tlv_raw(response, TLV_TYPE_IP, addr, addr_length);
-		packet_add_tlv_uint(response, TLV_TYPE_ADDR_TYPE, addr_type);
+		iResult = resolve_host(hostname, ai_family, &raw);
+		if (iResult == NO_ERROR)
+		{
+			packet_add_tlv_raw(response, TLV_TYPE_IP, &raw, sizeof(raw));
+			packet_add_tlv_uint(response, TLV_TYPE_ADDR_TYPE, ai_family);
+		}
 	}
 
-	packet_transmit_response(result, remote, response);
+	packet_transmit_response(iResult, remote, response);
 	return ERROR_SUCCESS;
 }
 
@@ -71,21 +92,27 @@ DWORD request_resolve_hosts(Remote *remote, Packet *packet)
 {
 	Packet *response = packet_create_response(packet);
 	Tlv hostname = {0};
-	DWORD result = NULL;
 	int index = 0;
+	int iResult;
 
 	while( packet_enum_tlv( packet, index++, TLV_TYPE_HOST_NAME, &hostname ) == ERROR_SUCCESS )
 	{
-		char addr[16] = {0};
-		u_short addr_type = 0;
-		u_short addr_length = 0;
+		struct in_addr raw = {0};
+		u_short ai_family = AF_INET;
 
-		resolve_host((LPCSTR)hostname.buffer, addr, &addr_type, &addr_length);
+		iResult = resolve_host((LPCSTR)hostname.buffer, ai_family, &raw);
 
-		packet_add_tlv_raw(response, TLV_TYPE_IP, addr, addr_length);
-		packet_add_tlv_uint(response, TLV_TYPE_ADDR_TYPE, addr_type);
+		if (iResult == NO_ERROR)
+		{
+			packet_add_tlv_raw(response, TLV_TYPE_IP, &raw, sizeof(raw));
+		}
+		else
+		{
+			packet_add_tlv_raw(response, TLV_TYPE_IP, NULL, 0);
+		}
+		packet_add_tlv_uint(response, TLV_TYPE_ADDR_TYPE, ai_family);
 	}
 
-	packet_transmit_response(result, remote, response);
+	packet_transmit_response(NO_ERROR, remote, response);
 	return ERROR_SUCCESS;
 }
