46f64a8f69c211678f9c015f970017cea6145759
OJ <oj@buffered.io>
2013-10-29 19:48:40 +1000

    Update incognito to v2
    Pulled the latest version of the incognito code from:
    http://labs.mwrinfosecurity.com/blog/2012/07/18/incognito-v2-0-released/
    
    This included a fix for Windows 2003 x64, which was reported as a bug in
    RM 8281.

--- c/meterpreter/source/extensions/incognito/list_tokens.h
+++ c/meterpreter/source/extensions/incognito/list_tokens.h
@@ -24,8 +24,21 @@ typedef enum
 	BY_GROUP
 } TOKEN_ORDER;
 
-SavedToken *get_token_list(DWORD *num_tokens_enum);
-void list_unique_tokens(TOKEN_ORDER);
+typedef struct
+{
+	BOOL SE_ASSIGNPRIMARYTOKEN_PRIVILEGE;
+	BOOL SE_CREATE_TOKEN_PRIVILEGE; 
+	BOOL SE_TCB_PRIVILEGE; 
+	BOOL SE_TAKE_OWNERSHIP_PRIVILEGE; 
+	BOOL SE_BACKUP_PRIVILEGE; 
+	BOOL SE_RESTORE_PRIVILEGE; 
+	BOOL SE_DEBUG_PRIVILEGE; 
+	BOOL SE_IMPERSONATE_PRIVILEGE; 
+	BOOL SE_RELABEL_PRIVILEGE; 
+	BOOL SE_LOAD_DRIVER_PRIVILEGE; 
+} TOKEN_PRIVS;
+
+SavedToken *get_token_list(DWORD *num_tokens_enum, TOKEN_PRIVS *token_privs);
 void process_user_token(HANDLE, unique_user_token*, DWORD*, TOKEN_ORDER);
 
 #endif
