22975bd716a6da08d3fbe1da565b0d00d469630b
jvazquez-r7 <juan_vazquez@rapid7.com>
2015-01-01 19:04:10 -0600

    Require getsid only on windows

--- c/meterpreter/source/extensions/stdapi/server/stdapi.c
+++ c/meterpreter/source/extensions/stdapi/server/stdapi.c
@@ -105,7 +105,6 @@ Command customCommands[] =
 
 	// Sys/config
 	COMMAND_REQ("stdapi_sys_config_getuid", request_sys_config_getuid),
-	COMMAND_REQ("stdapi_sys_config_getsid", request_sys_config_getsid),
 	COMMAND_REQ("stdapi_sys_config_sysinfo", request_sys_config_sysinfo),
 	COMMAND_REQ("stdapi_sys_config_rev2self", request_sys_config_rev2self),
 	COMMAND_REQ("stdapi_sys_config_getprivs", request_sys_config_getprivs),
@@ -113,6 +112,7 @@ Command customCommands[] =
 #ifdef _WIN32
 	COMMAND_REQ("stdapi_sys_config_steal_token", request_sys_config_steal_token),
 	COMMAND_REQ("stdapi_sys_config_drop_token", request_sys_config_drop_token),
+	COMMAND_REQ("stdapi_sys_config_getsid", request_sys_config_getsid),
 #endif
 
 	// Net
