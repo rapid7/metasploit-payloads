65058c6dbaa51bbf895a9d7c64d81a5e7614b126
Brent Cook <bcook@rapid7.com>
2015-01-30 18:43:55 -0600

    update sample module code and provide property defaults

--- java/meterpreter/extension-archetype/src/main/resources/archetype-resources/__pluginName__.rb
+++ java/meterpreter/extension-archetype/src/main/resources/archetype-resources/__pluginName__.rb
@@ -1,37 +1,48 @@
+# -*- coding: binary -*-
+
 module Rex
-module Post
-module Meterpreter
-module Extensions
-module ${pluginName.substring(0,1).toUpperCase()}${pluginName.substring(1)}
+  module Post
+    module Meterpreter
+      module Extensions
+        # This module contains a 'Hello World' meterpreter extension
+        module ${pluginName.substring(0,1).toUpperCase()}${pluginName.substring(1)}
+          TLV_TYPE_GREETEE = TLV_META_TYPE_STRING | (TLV_EXTENSIONS + 1)
 
-TLV_TYPE_GREETEE = TLV_META_TYPE_STRING | (TLV_EXTENSIONS + 1);
+          # This module implements a 'Hello World' meterpreter extension
+          class ${pluginName.substring(0,1).toUpperCase()}${pluginName.substring(1)} < Extension
+            def initialize(client)
+              super(client, '${pluginName}')
 
-class ${pluginName.substring(0,1).toUpperCase()}${pluginName.substring(1)} < Extension
+              client.register_extension_aliases(
+                [
+                  {
+                    'name' => '${pluginName}',
+                    'ext'  => self
+                  }
+                ])
+            end
 
-	def initialize(client)
-		super(client, '${pluginName}')
+            # Sends a greet_world request and gets a reply
+            #
+            # @return [String]
+            def ${pluginName}_greet_world
+              request = Packet.create_request('${pluginName}_greet_world')
+              response = client.send_request(request)
+              response.get_tlv_value(TLV_TYPE_STRING)
+            end
 
-		client.register_extension_aliases(
-			[
-				{
-					'name' => '${pluginName}',
-					'ext'  => self
-				}
-			])
-	end
-
-	def ${pluginName}_greet_world()
-		request = Packet.create_request('${pluginName}_greet_world')
-		response = client.send_request(request)
-		return response.get_tlv_value(TLV_TYPE_STRING)
-	end
-
-	def ${pluginName}_greet_someone(greetee)
-		request = Packet.create_request('${pluginName}_greet_someone')
-		request.add_tlv(TLV_TYPE_GREETEE, greetee)
-		response = client.send_request(request)
-		return response.get_tlv_value(TLV_TYPE_STRING)
-	end
+            # Sends a greet_someone request and gets a reply
+            #
+            # @return [String]
+            def ${pluginName}_greet_someone(greetee)
+              request = Packet.create_request('${pluginName}_greet_someone')
+              request.add_tlv(TLV_TYPE_GREETEE, greetee)
+              response = client.send_request(request)
+              response.get_tlv_value(TLV_TYPE_STRING)
+            end
+          end
+        end
+      end
+    end
+  end
 end
-
-end; end; end; end; end
