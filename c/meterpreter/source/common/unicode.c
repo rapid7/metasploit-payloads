7e017d4771e5c5a956457c43b76750de664a66b9
Brent Cook <bcook@rapid7.com>
2015-03-31 12:10:07 -0500

    move unicode helper functions to a common place

---
+++ c/meterpreter/source/common/unicode.c
@@ -0,0 +1,48 @@
+/*!
+ * @file unicode.c
+ * @brief Unicode conversion functions
+ */
+
+#include "common.h"
+
+wchar_t *utf8_to_wchar(const char *in)
+{
+	wchar_t *out;
+	int len = MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, in, -1, NULL, 0);
+	if (len <= 0) {
+		return NULL;
+	}
+
+	out = calloc(len, sizeof(wchar_t));
+	if (out == NULL) {
+		return NULL;
+	}
+
+	if (MultiByteToWideChar(CP_UTF8, 0, in, -1, out, len) == 0) {
+		free(out);
+		out = NULL;
+	}
+
+	return out;
+}
+
+char *wchar_to_utf8(const wchar_t *in)
+{
+	char *out;
+	int len = WideCharToMultiByte(CP_UTF8, 0, in, -1, NULL, 0, NULL, NULL);
+	if (len <= 0) {
+		return NULL;
+	}
+
+	out = calloc(len, sizeof(char));
+	if (out == NULL) {
+		return NULL;
+	}
+
+	if (WideCharToMultiByte(CP_UTF8, 0, in, -1, out, len, NULL, FALSE) == 0) {
+		free(out);
+		out = NULL;
+	}
+
+	return out;
+}
--- c/meterpreter/source/extensions/stdapi/server/fs/fs_win.c
+++ c/meterpreter/source/extensions/stdapi/server/fs/fs_win.c
@@ -3,48 +3,6 @@
 #include "fs_local.h"
 #include "precomp.h"
 
-static wchar_t *utf8_to_wchar(const char *in)
-{
-	wchar_t *out;
-	int len = MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, in, -1, NULL, 0);
-	if (len <= 0) {
-		return NULL;
-	}
-
-	out = calloc(len, sizeof(wchar_t));
-	if (out == NULL) {
-		return NULL;
-	}
-
-	if (MultiByteToWideChar(CP_UTF8, 0, in, -1, out, len) == 0) {
-		free(out);
-		out = NULL;
-	}
-
-	return out;
-}
-
-static char *wchar_to_utf8(const wchar_t *in)
-{
-	char *out;
-	int len = WideCharToMultiByte(CP_UTF8, 0, in, -1, NULL, 0, NULL, NULL);
-	if (len <= 0) {
-		return NULL;
-	}
-
-	out = calloc(len, sizeof(char));
-	if (out == NULL) {
-		return NULL;
-	}
-
-	if (WideCharToMultiByte(CP_UTF8, 0, in, -1, out, len, NULL, FALSE) == 0) {
-		free(out);
-		out = NULL;
-	}
-
-	return out;
-}
-
 char * fs_expand_path(const char *regular)
 {
 	wchar_t expanded_path[FS_MAX_PATH];
