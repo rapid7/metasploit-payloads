479b501feca3d816f2dbfccd2144b14b852b1a51
OJ <oj@buffered.io>
2015-04-28 14:03:24 +1000

    Remove commented out stuff from header files

--- c/meterpreter/source/server/win/server_transport_tcp.h
+++ c/meterpreter/source/server/win/server_transport_tcp.h
@@ -2,6 +2,5 @@
 #define _METERPRETER_SERVER_SETUP_TCP
 
 Transport* transport_create_tcp_config(MetsrvTransportTcp* tcpConfig);
-Transport* transport_create_tcp(wchar_t* url, TimeoutSettings* timeouts);
 
 #endif
--- c/meterpreter/source/server/win/server_transport_winhttp.h
+++ c/meterpreter/source/server/win/server_transport_winhttp.h
@@ -1,8 +1,6 @@
 #ifndef _METERPRETER_SERVER_SETUP_WINHTTP
 #define _METERPRETER_SERVER_SETUP_WINHTTP
 
-//Transport* transport_create_http(BOOL ssl, wchar_t* url, wchar_t* ua, wchar_t* proxy,
-	//wchar_t* proxyUser, wchar_t* proxyPass, PBYTE certHash, TimeoutSettings* timeouts);
 Transport* transport_create_http_config(MetsrvTransportHttp* httpConfig);
 
 #endif
