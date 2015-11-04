578ac70fd943d2e406b4396b9010d0a06ac4d122
OJ <oj@buffered.io>
2015-11-04 14:37:57 +1000

    Add transport add command to python binding

--- c/meterpreter/source/extensions/python/Lib/meterpreter/transport.py
+++ c/meterpreter/source/extensions/python/Lib/meterpreter/transport.py
@@ -9,6 +9,9 @@ def list():
   if resp == None:
       return []
 
+  if packet_get_tlv(resp, TLV_TYPE_RESULT)['value'] != 0:
+    return []
+
   transports = []
   for transport in packet_enum_tlvs(resp, TLV_TYPE_TRANS_GROUP):
     t = transport['value']
@@ -30,3 +33,35 @@ def list():
     'SessionExpiry': expiry,
     'Transports': transports
   }
+
+def add(url, session_expiry=None, comm_timeout=None, retry_total=None,
+  retry_wait=None, ua=None, proxy_host=None, proxy_user=None,
+  proxy_pass=None, cert_hash=None):
+
+  tlv = tlv_pack(TLV_TYPE_TRANS_URL, url)
+
+  if session_expiry:
+    tlv += tlv_pack(TLV_TYPE_TRANS_SESSION_EXP, session_expiry)
+  if comm_timeout:
+    tlv += tlv_pack(TLV_TYPE_TRANS_COMM_TIMEOUT, comm_timeout)
+  if retry_total:
+    tlv += tlv_pack(TLV_TYPE_TRANS_RETRY_TOTAL, retry_total)
+  if retry_wait:
+    tlv += tlv_pack(TLV_TYPE_TRANS_RETRY_WAIT, retry_wait)
+  if ua:
+    tlv += tlv_pack(TLV_TYPE_TRANS_UA, ua)
+  if proxy_host:
+    tlv += tlv_pack(TLV_TYPE_TRANS_PROXY_HOST, proxy_host)
+  if proxy_user:
+    tlv += tlv_pack(TLV_TYPE_TRANS_PROXY_USER, proxy_user)
+  if proxy_pass:
+    tlv += tlv_pack(TLV_TYPE_TRANS_PROXY_PASS, proxy_pass)
+  if cert_hash:
+    tlv += tlv_pack(TLV_TYPE_TRANS_CERT_HASH, cert_hash)
+
+  resp = invoke_meterpreter('core_transport_add', True, tlv)
+  if resp == None:
+    return False
+
+  return packet_get_tlv(resp, TLV_TYPE_RESULT)['value'] == 0
+
