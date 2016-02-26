58d4d21c744b10d46295a1ce5f4a2b0994803537
Rohit Shampur <stormtheh4ck3r@users.noreply.github.com>
2016-02-26 16:14:40 +0530

    Created toast_android.java

---
+++ java/androidpayload/library/src/com/metasploit/meterpreter/android/toast_android.java
@@ -0,0 +1,20 @@
+package com.metasploit.meterpreter.android;
+
+import android.telephony.SmsManager;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.app.Activity;
+import android.content.Context;
+
+import com.metasploit.meterpreter.AndroidMeterpreter;
+import com.metasploit.meterpreter.Meterpreter;
+import com.metasploit.meterpreter.TLVPacket;
+import com.metasploit.meterpreter.command.Command;
+
+public class send_sms_android implements Command {
+  
+
+}
