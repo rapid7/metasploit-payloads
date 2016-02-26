92a215252ed161fe3f2b8da73de39ada88fed5cb
Rohit Shampur <stormtheh4ck3r@users.noreply.github.com>
2016-02-26 16:38:44 +0530

    Updated toast_android.java

--- java/androidpayload/library/src/com/metasploit/meterpreter/android/toast_android.java
+++ java/androidpayload/library/src/com/metasploit/meterpreter/android/toast_android.java
@@ -1,12 +1,6 @@
 package com.metasploit.meterpreter.android;
 
-import android.telephony.SmsManager;
-
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.app.Activity;
+import android.widget.Toast;
 import android.content.Context;
 
 import com.metasploit.meterpreter.AndroidMeterpreter;
@@ -14,7 +8,25 @@ import com.metasploit.meterpreter.Meterpreter;
 import com.metasploit.meterpreter.TLVPacket;
 import com.metasploit.meterpreter.command.Command;
 
-public class send_sms_android implements Command {
+public class toast_android implements Command {
+  
+  private static final int TLV_EXTENSIONS = 20000;
+  private static final int TLV_TYPE_TOAST_STRING = TLVPacket.TLV_META_TYPE_STRING
+            | (TLV_EXTENSIONS + 9001);
+  private static final int TLV_TYPE_TOAST_SR = TLVPacket.TLV_META_TYPE_STRING
+            | (TLV_EXTENSIONS + 9021);
+  
+   @Override
+    public int execute(Meterpreter meterpreter, TLVPacket request,
+                       TLVPacket response) throws Exception {
+        String message = request.getStringValue(TLV_TYPE_TOAST_STRING);
+        String TOAST_SUCCESS = "SUCCESS";
+        AndroidMeterpreter androidMeterpreter = (AndroidMeterpreter) meterpreter;
+        final Context context = androidMeterpreter.getContext();
+        Toast.makeText(context,message,Toast.LENGTH_LONG).show();
+        response.addOverflow(TLV_TYPE_TOAST_SR, TOAST_SUCCESS);
+        return ERROR_SUCCESS;
+    }
   
 
 }
