f0b9f6b76ab0048d9f9c4e2206f2fadf1a0e77f3
Brent Cook <busterb@gmail.com>
2017-01-12 21:28:49 -0600

    replace timestomp code with rewritten versions

--- c/meterpreter/source/extensions/priv/server/fs.c
+++ c/meterpreter/source/extensions/priv/server/fs.c
@@ -1,7 +1,8 @@
 #include "precomp.h"
 
-// Import code from timestomp
-#include "timestomp.c"
+#include <windows.h>
+#include <stdio.h>
+#include "defs.h"
 
 #define EpochTimeToSystemTime(epoch, sys) \
 	{ \
@@ -29,70 +30,266 @@
 		*(epoch) = mktime(&et); \
 	}
 
+int SetFileBasicInfo(HANDLE file, FILE_BASIC_INFORMATION *file_info)
+{
+	HMODULE ntdll_handle = GetModuleHandle("ntdll.dll");
+	if (ntdll_handle == NULL) {
+		return -1;
+	}
+
+	NTSETINFORMATIONFILE NtSetInformationFile =
+		(NTSETINFORMATIONFILE)GetProcAddress(ntdll_handle, "NtSetInformationFile");
+	if (NtSetInformationFile == NULL) {
+		return -1;
+	}
+
+	IO_STATUS_BLOCK status_block = {0};
+	return NtSetInformationFile(file, &status_block, file_info,
+			sizeof *file_info, FileBasicInformation);
+}
+
+int GetFileBasicInfo(HANDLE file, FILE_BASIC_INFORMATION *file_info)
+{
+	HMODULE ntdll_handle = GetModuleHandle("ntdll.dll");
+	if (ntdll_handle == NULL) {
+		return -1;
+	}
+
+	NTQUERYINFORMATIONFILE NtQueryInformationFile =
+		(NTQUERYINFORMATIONFILE)GetProcAddress(ntdll_handle, "NtQueryInformationFile");
+	if (NtQueryInformationFile == NULL) {
+		return -1;
+	}
+
+	IO_STATUS_BLOCK status_block = {0};
+	return NtQueryInformationFile(file, &status_block, file_info,
+			sizeof *file_info, FileBasicInformation);
+}
+
+HANDLE FileOpen(char *filename)
+{
+	if (filename == NULL) {
+		return NULL;
+	}
+
+	wchar_t *name = utf8_to_wchar(filename);
+	if (name == NULL) {
+		return NULL;
+	}
+
+	HANDLE file = CreateFileW(name,
+		FILE_READ_ATTRIBUTES | FILE_WRITE_ATTRIBUTES,
+		0, NULL, OPEN_EXISTING, 0, NULL);
+
+	free(name);
+	return file;
+}
+
+void FileClose(HANDLE file)
+{
+	if (file) {
+		CloseHandle(file);
+	}
+}
+
+int SystemTimeToLargeInteger(SYSTEMTIME in, LARGE_INTEGER *out) {
+
+	FILETIME filetime;
+	FILETIME utcfiletime;
+
+    if (SystemTimeToFileTime(&in, &filetime) == 0 ||
+			LocalFileTimeToFileTime(&filetime, &utcfiletime) == 0) {
+		return -1;
+	}
+
+	out->LowPart = utcfiletime.dwLowDateTime;
+	out->HighPart = utcfiletime.dwHighDateTime;
+
+	return 0;
+}
+
+int LargeIntegerToSystemTime(SYSTEMTIME *localsystemtime, LARGE_INTEGER largeinteger) {
+
+	FILETIME localfiletime;
+	FILETIME filetime = {
+		.dwLowDateTime = largeinteger.LowPart,
+		.dwHighDateTime = largeinteger.HighPart
+	};
+
+	if (FileTimeToLocalFileTime(&filetime, &localfiletime) == 0 ||
+    		FileTimeToSystemTime(&localfiletime, localsystemtime) == 0) {
+		return -1;
+	}
+
+	return 0;
+}
+
+int SetFileTimes(HANDLE file, SYSTEMTIME time)
+{
+	FILE_BASIC_INFORMATION fbi;
+	if (GetFileBasicInfo(file, &fbi)) {
+		return -1;
+	}
+
+	LARGE_INTEGER time_int;
+	if (SystemTimeToLargeInteger(time, &time_int)) {
+		return -1;
+	}
+
+	fbi.ChangeTime = time_int;
+	fbi.CreationTime = time_int;
+	fbi.LastAccessTime = time_int;
+	fbi.LastWriteTime = time_int;
+
+	if (SetFileBasicInfo(file, &fbi)) {
+		return -1;
+	}
+
+	return 0;
+}
+
+#define FS_MAX_PATH  32768
+
+int SetDirectoryTimes(wchar_t * directory, SYSTEMTIME time)
+{
+	wchar_t firstFile[FS_MAX_PATH];
+	swprintf_s(firstFile, FS_MAX_PATH, L"%s\\*", directory);
+
+	WIN32_FIND_DATAW data;
+	HANDLE file = FindFirstFileW(firstFile, &data);
+	if (file != INVALID_HANDLE_VALUE) {
+		do {
+			if (wcscmp(data.cFileName, L".") && wcscmp(data.cFileName, L"..")) {
+				/*
+				 * Set the times for all entries in this directory
+				 */
+				HANDLE file = CreateFileW(data.cFileName,
+					FILE_READ_ATTRIBUTES | FILE_WRITE_ATTRIBUTES,
+					0, NULL, OPEN_EXISTING, 0, NULL);
+				if (file) {
+					SetFileTimes(file, time);
+					CloseHandle(file);
+				}
+			}
+		} while (FindNextFileW(file, &data) != 0);
+
+		FindClose(file);
+	} else {
+		if (GetLastError() != ERROR_FILE_NOT_FOUND) {
+			return GetLastError();
+		}
+	}
+
+	return 0;
+}
+
+int SetDirectoryTimesRecursive(wchar_t *directory, SYSTEMTIME time, int depth)
+{
+	DWORD rc = ERROR_SUCCESS;
+	BOOL searched = FALSE;
+	WIN32_FIND_DATAW find_data = {0};
+	size_t len = wcslen(directory) + 5;
+
+	if (depth > 32 || len >= FS_MAX_PATH) {
+		return ERROR_SUCCESS;
+	}
+
+	wchar_t *firstFile = calloc(len, sizeof(wchar_t));
+	if (!firstFile) {
+		return ERROR_SUCCESS;
+	}
+
+	swprintf_s(firstFile, FS_MAX_PATH, L"%s\\*.*", directory);
+
+	HANDLE file = FindFirstFileW(firstFile, &find_data);
+	if (file != INVALID_HANDLE_VALUE) {
+		do {
+			if (find_data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+				if (wcscmp(find_data.cFileName, L".") && wcscmp(find_data.cFileName, L"..")) {
+					size_t len = wcslen(directory) + wcslen(find_data.cFileName) + 32;
+					wchar_t *nextDirectory = calloc(len, sizeof(wchar_t));
+					if (nextDirectory) {
+						swprintf_s(nextDirectory, len, L"%s\\%s", directory, find_data.cFileName);
+						rc = SetDirectoryTimesRecursive(nextDirectory, time, depth + 1);
+						free(nextDirectory);
+					}
+				}
+			} else if (!searched) {
+				rc = SetDirectoryTimes(directory, time);
+				searched = TRUE;
+			}
+		} while (FindNextFileW(file, &find_data) != 0);
+
+		FindClose(file);
+
+	} else {
+		if (GetLastError() != ERROR_FILE_NOT_FOUND) {
+			rc = GetLastError();
+		}
+	}
+
+	free(firstFile);
+	return rc;
+}
 
 DWORD request_fs_get_file_mace(Remote *remote, Packet *packet)
 {
-	FILE_BASIC_INFORMATION fbi;
 	SYSTEMTIME lt;
 	Packet *response = packet_create_response(packet);
-	HANDLE file = NULL;
-	PCHAR filePath = packet_get_tlv_value_string(packet, TLV_TYPE_FS_FILE_PATH);
+	HANDLE file = FileOpen(packet_get_tlv_value_string(packet, TLV_TYPE_FS_FILE_PATH));
+
+	if (file == NULL) {
+		SetLastError(ERROR_INVALID_PARAMETER);
+		goto err;
+	}
+
+	FILE_BASIC_INFORMATION fbi;
 	struct {
 		LARGE_INTEGER *ft;
 		unsigned long tlv;
 	} fields[] = {
-		{ &fbi.LastWriteTime,  TLV_TYPE_FS_FILE_MODIFIED  },
-		{ &fbi.LastAccessTime, TLV_TYPE_FS_FILE_ACCESSED  },
-		{ &fbi.CreationTime,   TLV_TYPE_FS_FILE_CREATED   },
-		{ &fbi.ChangeTime,     TLV_TYPE_FS_FILE_EMODIFIED },
+		{ &fbi.LastWriteTime, TLV_TYPE_FS_FILE_MODIFIED },
+		{ &fbi.LastAccessTime, TLV_TYPE_FS_FILE_ACCESSED },
+		{ &fbi.CreationTime, TLV_TYPE_FS_FILE_CREATED },
+		{ &fbi.ChangeTime, TLV_TYPE_FS_FILE_EMODIFIED },
 	};
-	int x;
 
-	do
-	{
-		// Invalid file path, bail.
-		if (!filePath)
-		{
-			SetLastError(ERROR_INVALID_PARAMETER);
-			break;
-		}
+	if (GetFileBasicInfo(file, &fbi) != 0) {
+		goto err;
+	}
 
-		// If we fail to retrieve basic information, bail.
-		if (!(file = RetrieveFileBasicInformation(filePath, &fbi)))
-			break;
+	for (int i = 0; i < 4; i++) {
+		time_t epoch = 0;
 
-		// Convert the time for each field
-		for (x = 0; x < (sizeof(fields) / sizeof(fields[0])); x++)
-		{
-			time_t epoch = 0;
-
-			if (ConvertLargeIntegerToLocalTime(&lt, *fields[x].ft) != 1)
-				break;
-
+		if (LargeIntegerToSystemTime(&lt, *fields[i].ft) == 0) {
 			SystemTimeToEpochTime(&lt, &epoch);
-
-			packet_add_tlv_uint(response, fields[x].tlv, (UINT)epoch);
+			packet_add_tlv_uint(response, fields[i].tlv, (UINT)epoch);
 		}
-		
-		SetLastError(ERROR_SUCCESS);
+	}
 
-	} while (0);
+	SetLastError(ERROR_SUCCESS);
 
-	// Close the file handle.
-	if (file)
-		CloseHandle(file);
-
+err:
+	FileClose(file);
 	packet_transmit_response(GetLastError(), remote, response);
-
 	return ERROR_SUCCESS;
 }
 
 DWORD request_fs_set_file_mace(Remote *remote, Packet *packet)
 {
-	FILE_BASIC_INFORMATION fbi;
 	Packet *response = packet_create_response(packet);
-	HANDLE file = NULL;
-	PCHAR filePath = packet_get_tlv_value_string(packet, TLV_TYPE_FS_FILE_PATH);
+
+	HANDLE file = FileOpen(packet_get_tlv_value_string(packet, TLV_TYPE_FS_FILE_PATH));
+	if (!file) {
+		SetLastError(ERROR_INVALID_PARAMETER);
+		goto out;
+	}
+
+	FILE_BASIC_INFORMATION fbi;
+	if (GetFileBasicInfo(file, &fbi) != 0) {
+		goto out;
+	}
+
 	struct {
 		LARGE_INTEGER *ft;
 		unsigned long tlv;
@@ -102,145 +299,115 @@ DWORD request_fs_set_file_mace(Remote *remote, Packet *packet)
 		{ &fbi.CreationTime,   TLV_TYPE_FS_FILE_CREATED   },
 		{ &fbi.ChangeTime,     TLV_TYPE_FS_FILE_EMODIFIED },
 	};
-	int x;
-
-	do
-	{
-		// Invalid file path, bail.
-		if (!filePath)
-		{
-			SetLastError(ERROR_INVALID_PARAMETER);
-			break;
-		}
-
-		// If we fail to retrieve basic information, bail.
-		if (!(file = RetrieveFileBasicInformation(filePath, &fbi)))
-			break;
-
-		// If the TLV for the associated field is supplied, update it.
-		for (x = 0; x < (sizeof(fields) / sizeof(fields[0])); x++)
-		{
+	for (int i = 0; i < 4; i++) {
+		time_t epoch = packet_get_tlv_value_uint(packet, (TlvType)fields[i].tlv);
+		if (epoch) {
 			SYSTEMTIME st;
-			time_t epoch = packet_get_tlv_value_uint(packet, (TlvType)fields[x].tlv);
-
-			if (!epoch)
-				continue;
-
 			EpochTimeToSystemTime(epoch, &st);
-
-			// Conversion failed, that sucks.
-			if (ConvertLocalTimeToLargeInteger(st, fields[x].ft) == 0)
-				break;
+			SystemTimeToLargeInteger(st, fields[i].ft);
 		}
+	}
 
-		// If we fail to set the MACE, bail.
-		if (SetFileMACE(file, fbi) == 0)
-			break;
+	if (SetFileBasicInfo(file, &fbi)) {
+		goto out;
+	}
 
-		SetLastError(ERROR_SUCCESS);
+	SetLastError(ERROR_SUCCESS);
 
-	} while (0);
-
-	// Close the file handle.
-	if (file)
-		CloseHandle(file);
-
+out:
+	FileClose(file);
 	packet_transmit_response(GetLastError(), remote, response);
-
 	return ERROR_SUCCESS;
 }
 
 DWORD request_fs_set_file_mace_from_file(Remote *remote, Packet *packet)
 {
-	FILE_BASIC_INFORMATION fbi;
 	Packet *response = packet_create_response(packet);
-	PCHAR tgtFilePath = packet_get_tlv_value_string(packet, TLV_TYPE_FS_FILE_PATH);
-	PCHAR srcFilePath = packet_get_tlv_value_string(packet, TLV_TYPE_FS_SRC_FILE_PATH);
-	HANDLE srcFile = NULL, tgtFile = NULL;
-	ULONG attributes;
 
-	do
-	{
-		// Are we missing something?
-		if (!tgtFilePath || !srcFilePath)
-		{
-			SetLastError(ERROR_INVALID_PARAMETER);
-			break;
-		}
+	HANDLE tgtFile = FileOpen(packet_get_tlv_value_string(packet, TLV_TYPE_FS_FILE_PATH));
+	HANDLE srcFile = FileOpen(packet_get_tlv_value_string(packet, TLV_TYPE_FS_SRC_FILE_PATH));
+	if (tgtFile == NULL || srcFile == NULL) {
+		goto out;
+	}
 
-		// Get info.
-		if (!(tgtFile = RetrieveFileBasicInformation(tgtFilePath, &fbi)))
-			break;
-		attributes = fbi.FileAttributes;
-		if (!(srcFile = RetrieveFileBasicInformation(srcFilePath, &fbi)))
-			break;
-		fbi.FileAttributes = attributes;
+	FILE_BASIC_INFORMATION fbi;
+	if (GetFileBasicInfo(tgtFile, &fbi) != 0) {
+		goto out;
+	}
 
-		if (SetFileMACE(tgtFile, fbi) == 0)
-			break;
+	ULONG attributes = fbi.FileAttributes;
 
-		SetLastError(ERROR_SUCCESS);
+	if (GetFileBasicInfo(srcFile, &fbi) != 0) {
+		goto out;
+	}
 
-	} while (0);
+	fbi.FileAttributes = attributes;
 
-	if (srcFile)
-		CloseHandle(srcFile);
-	if (tgtFile)
-		CloseHandle(tgtFile);
+	if (SetFileBasicInfo(tgtFile, &fbi) != 0) {
+		goto out;
+	}
+
+	SetLastError(ERROR_SUCCESS);
+
+out:
+	FileClose(srcFile);
+	FileClose(tgtFile);
 
 	packet_transmit_response(GetLastError(), remote, response);
-
 	return ERROR_SUCCESS;
 }
+
+static SYSTEMTIME epoch = {
+	.wYear = 1601,
+	.wMonth = 1,
+	.wDayOfWeek = 0,
+	.wDay = 1,
+	.wHour = 0,
+	.wMinute = 0,
+	.wSecond = 0,
+	.wMilliseconds = 0
+};
 
 DWORD request_fs_blank_file_mace(Remote *remote, Packet *packet)
 {
 	Packet *response = packet_create_response(packet);
-	PCHAR filePath = packet_get_tlv_value_string(packet, TLV_TYPE_FS_FILE_PATH);
 
-	do
-	{
-		// Are we missing something?
-		if (!filePath)
-		{
-			SetLastError(ERROR_INVALID_PARAMETER);
-			break;
-		}
+	HANDLE file = FileOpen(packet_get_tlv_value_string(packet, TLV_TYPE_FS_FILE_PATH));
+	if (!file) {
+		SetLastError(ERROR_INVALID_PARAMETER);
+		goto out;
+	}
 
-		if (SetMinimumTimeValues(filePath) == 0)
-			break;
+	if (SetFileTimes(file, epoch) != 0) {
+		goto out;
+	}
 
-		SetLastError(ERROR_SUCCESS);
+	SetLastError(ERROR_SUCCESS);
 
-	} while (0);
-
+out:
+	FileClose(file);
 	packet_transmit_response(GetLastError(), remote, response);
-
 	return ERROR_SUCCESS;
 }
 
 DWORD request_fs_blank_directory_mace(Remote *remote, Packet *packet)
 {
 	Packet *response = packet_create_response(packet);
-	PCHAR filePath = packet_get_tlv_value_string(packet, TLV_TYPE_FS_FILE_PATH);
+	wchar_t *filePath = utf8_to_wchar(packet_get_tlv_value_string(packet, TLV_TYPE_FS_FILE_PATH));
 
-	do
-	{
-		// Are we missing something?
-		if (!filePath)
-		{
-			SetLastError(ERROR_INVALID_PARAMETER);
-			break;
-		}
+	if (filePath == NULL) {
+		SetLastError(ERROR_INVALID_PARAMETER);
+		goto out;
+	}
 
-		if (TheCraigOption(filePath) == 0)
-			break;
+	if (SetDirectoryTimesRecursive(filePath, epoch, 0) != 0) {
+		goto out;
+	}
 
-		SetLastError(ERROR_SUCCESS);
+	SetLastError(ERROR_SUCCESS);
 
-	} while (0);
-
+out:
+	free(filePath);
 	packet_transmit_response(GetLastError(), remote, response);
-
 	return ERROR_SUCCESS;
 }
