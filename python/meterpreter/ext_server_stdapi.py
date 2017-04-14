7db0847d87f0ec88dbec11d6cefbaf7f5dea2bc1
Ouahib El Hanchi <ouahib.el.hanchi@gmail.com>
2017-04-14 06:44:28 +0100

    Fixed whitespace, can't seem to get them right

--- python/meterpreter/ext_server_stdapi.py
+++ python/meterpreter/ext_server_stdapi.py
@@ -241,23 +241,23 @@ if has_ctypes:
 	class UNIVERSAL_NAME_INFO(ctypes.Structure):
 		_fields_ = [("lpUniversalName", ctypes.c_char_p)]
 
-    class EVENTLOGRECORD(ctypes.Structure):
-        _fields_ = [("Length", ctypes.c_uint32),
-            ("Reserved", ctypes.c_uint32),
-            ("RecordNumber", ctypes.c_uint32),
-            ("TimeGenerated", ctypes.c_uint32),
-            ("TimeWritten", ctypes.c_uint32),
-            ("EventID", ctypes.c_uint32),
-            ("EventType", ctypes.c_uint16),
-            ("NumStrings", ctypes.c_uint16),
-            ("EventCategory", ctypes.c_uint16),
-            ("ReservedFlags", ctypes.c_uint16),
-            ("ClosingRecordNumber", ctypes.c_uint32),
-            ("StringOffset", ctypes.c_uint32),
-            ("UserSidLength", ctypes.c_uint32),
-            ("UserSidOffset", ctypes.c_uint32),
-            ("DataLength", ctypes.c_uint32),
-            ("DataOffset", ctypes.c_uint32)]
+	class EVENTLOGRECORD(ctypes.Structure):
+		_fields_ = [("Length", ctypes.c_uint32),
+			("Reserved", ctypes.c_uint32),
+			("RecordNumber", ctypes.c_uint32),
+			("TimeGenerated", ctypes.c_uint32),
+			("TimeWritten", ctypes.c_uint32),
+			("EventID", ctypes.c_uint32),
+			("EventType", ctypes.c_uint16),
+			("NumStrings", ctypes.c_uint16),
+			("EventCategory", ctypes.c_uint16),
+			("ReservedFlags", ctypes.c_uint16),
+			("ClosingRecordNumber", ctypes.c_uint32),
+			("StringOffset", ctypes.c_uint32),
+			("UserSidLength", ctypes.c_uint32),
+			("UserSidOffset", ctypes.c_uint32),
+			("DataLength", ctypes.c_uint32),
+			("DataOffset", ctypes.c_uint32)]
 
 	#
 	# Linux Structures
