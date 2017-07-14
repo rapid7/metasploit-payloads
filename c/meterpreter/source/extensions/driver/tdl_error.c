09bd16eae80e82429789aaee9d8dd56f3d86c8cd
William Webb <william_webb@rapid7.com>
2017-07-14 02:21:59 -0500

    code wants to be non-redundant

--- c/meterpreter/source/extensions/driver/tdl_error.c
+++ c/meterpreter/source/extensions/driver/tdl_error.c
@@ -46,11 +46,10 @@ VOID tdlReportError(
 	errbuf = wchar_to_utf8(lpText);
 	if (errbuf) {
 		packet_add_tlv_raw(response, TLV_TYPE_TDL_ERROR_MSG, (LPVOID)errbuf, (DWORD)strlen(errbuf) + 1);
-		packet_transmit_response(result, remote, response);
 		free(errbuf);
 	}
 	else {
 		packet_add_tlv_string(response, TLV_TYPE_TDL_ERROR_MSG, "wtf !!");
-		packet_transmit_response(result, remote, response);
 	}
+	packet_transmit_response(result, remote, response);
 }
