fe02cd012b4c139e8a7013cb5c68cc1cba4b8a13
Tim <timrlw@gmail.com>
2017-09-07 18:18:22 +0800

    fix process_execute with escaped arguments

--- java/meterpreter/stdapi/src/main/java/com/metasploit/meterpreter/stdapi/stdapi_sys_process_execute_V1_3.java
+++ java/meterpreter/stdapi/src/main/java/com/metasploit/meterpreter/stdapi/stdapi_sys_process_execute_V1_3.java
@@ -3,8 +3,14 @@ package com.metasploit.meterpreter.stdapi;
 import java.io.IOException;
 
 public class stdapi_sys_process_execute_V1_3 extends stdapi_sys_process_execute {
-    protected Process execute(String[] cmdarray) throws IOException {
-        Process proc = Runtime.getRuntime().exec(cmdarray, null, Loader.cwd);
-        return proc;
+
+    @Override
+    protected Process execute(String cmdstr) throws IOException {
+        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+            return super.execute(cmdstr);
+        }
+        Process process = Runtime.getRuntime().exec(new String[]{"sh", "-c", cmdstr}, null, Loader.cwd);
+        return process;
     }
+
 }
