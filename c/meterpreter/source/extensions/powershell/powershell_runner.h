6163be1142c757e3f1f57e7ad283eb7b05e9cccf
OJ <oj@buffered.io>
2020-05-01 15:41:31 +1000

    Updated powershell extension with appropriate command map

--- c/meterpreter/source/extensions/powershell/powershell_runner.h
+++ c/meterpreter/source/extensions/powershell/powershell_runner.h
@@ -1,12 +1,12 @@
 /*!
   * @file powershell_runner.h
-  * @brief This file was generated at 04/28/2020 13:38:02 UTC, do not modify directly.
+  * @brief This file was generated at 05/01/2020 05:40:30 UTC, do not modify directly.
   */
 
  #ifndef _METERPRETER_SOURCE_EXTENSION_POWERSHELL_RUNNER_H
  #define _METERPRETER_SOURCE_EXTENSION_POWERSHELL_RUNNER_H
 
- #define PSHRUNNER_DLL_LEN 64000
+ #define PSHRUNNER_DLL_LEN 64512
 
  extern unsigned char PowerShellRunnerDll[PSHRUNNER_DLL_LEN];
 
