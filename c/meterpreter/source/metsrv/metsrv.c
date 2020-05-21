d820fb645841d735204d4070e86a8b4b0fbc62b5
OJ <oj@buffered.io>
2020-05-21 16:09:10 +1000

    Get x86 working for metsrv

--- c/meterpreter/source/metsrv/metsrv.c
+++ c/meterpreter/source/metsrv/metsrv.c
@@ -9,7 +9,6 @@
 
 
 #define REFLECTIVEDLLINJECTION_CUSTOM_DLLMAIN
-#define RDIDLL_NOEXPORT
 #include "../ReflectiveDLLInjection/dll/src/ReflectiveLoader.c"
 #include "../ReflectiveDLLInjection/inject/src/GetProcAddressR.c"
 #include "../ReflectiveDLLInjection/inject/src/LoadLibraryR.c"
