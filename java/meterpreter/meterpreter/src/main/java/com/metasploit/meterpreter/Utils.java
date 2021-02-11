61cd6dce8d8f9aa4d61aa4596013ee64d5c8230d
XenoAmess <xenoamess@gmail.com>
2021-02-11 22:24:58 +0800

    change c-style array declaration

--- java/meterpreter/meterpreter/src/main/java/com/metasploit/meterpreter/Utils.java
+++ java/meterpreter/meterpreter/src/main/java/com/metasploit/meterpreter/Utils.java
@@ -45,7 +45,7 @@ public class Utils {
     }
 
 
-    public static String bytesToHex(byte bytes[]) {
+    public static String bytesToHex(byte[] bytes) {
         char[] hexDigits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
         StringBuilder buf = new StringBuilder(bytes.length * 2);
         for (byte aByte : bytes) {
