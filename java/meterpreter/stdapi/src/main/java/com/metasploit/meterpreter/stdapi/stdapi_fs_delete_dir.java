e0dae30a95ca0c0b6325d15eca7f5f12f3c9b78e
Spencer McIntyre <zeroSteiner@gmail.com>
2022-01-05 17:12:41 -0500

    Update Javas stdapi_fs_delete_dir to be recursive

--- java/meterpreter/stdapi/src/main/java/com/metasploit/meterpreter/stdapi/stdapi_fs_delete_dir.java
+++ java/meterpreter/stdapi/src/main/java/com/metasploit/meterpreter/stdapi/stdapi_fs_delete_dir.java
@@ -9,16 +9,43 @@ import com.metasploit.meterpreter.TLVType;
 import com.metasploit.meterpreter.command.Command;
 
 public class stdapi_fs_delete_dir implements Command {
-
     public int execute(Meterpreter meterpreter, TLVPacket request, TLVPacket response) throws Exception {
         String path = request.getStringValue(TLVType.TLV_TYPE_DIRECTORY_PATH);
         File file = Loader.expand(path);
-        if (!file.exists() || !file.isDirectory()) {
+        if (isSymlink(file)) {
+            if (!file.delete()) {
+                throw new IOException("Cannot delete symbolic link " + file.getCanonicalPath());
+            }
+        } else if (file.isDirectory()) {
+            rmtree(file);
+        } else {
             throw new IOException("Directory not found: " + path);
         }
-        if (!file.delete()) {
-            throw new IOException("Cannot delete directory " + file.getCanonicalPath());
-        }
         return ERROR_SUCCESS;
+    }
+
+    private static boolean isSymlink(File file) throws IOException {
+        File canon;
+        if (file.getParent() == null) {
+            canon = file;
+        } else {
+            File canonDir = file.getParentFile().getCanonicalFile();
+            canon = new File(canonDir, file.getName());
+        }
+        return !canon.getCanonicalFile().equals(canon.getAbsoluteFile());
+    }
+
+    private void rmtree(File file) throws IOException {
+        for (File subFile : file.listFiles()) {
+            if (isSymlink(subFile)) {
+                subFile.delete();
+            } else if (subFile.isDirectory()) {
+                rmtree(subFile);
+            } else {
+                subFile.delete();
+            }
+        }
+        file.delete();
+        return;
     }
 }
