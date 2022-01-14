328a43bccf12ab66665fa515fc4af976fdd1c4c4
sjanusz <simon_janusz@rapid7.com>
2022-01-14 15:02:22 +0000

    Split getpid implementations into separate versions

--- java/meterpreter/stdapi/src/main/java/com/metasploit/meterpreter/stdapi/stdapi_sys_process_getpid_V1_5.java
+++ java/meterpreter/stdapi/src/main/java/com/metasploit/meterpreter/stdapi/stdapi_sys_process_getpid_V1_5.java
@@ -18,17 +18,7 @@ public class stdapi_sys_process_getpid_V1_5 implements Command {
     }
 
     public int execute(Meterpreter meterpreter, TLVPacket request, TLVPacket response) throws Exception {
-        if (classExists("java.lang.ProcessHandle"))
-        {
-            Class<?> processHandleClass = Class.forName("java.lang.ProcessHandle");
-            Method getCurrentProcessHandleMethod = processHandleClass.getMethod("current");
-            Object currentProcessHandle = getCurrentProcessHandleMethod.invoke(null);
-            Object pidObject = processHandleClass.getMethod("pid").invoke(currentProcessHandle);
-            Long pid = (Long) pidObject;
-            response.add(TLVType.TLV_TYPE_PID, pid.intValue());
-            return ERROR_SUCCESS;
-        }
-        else if (classExists("java.lang.management.ManagementFactory") && classExists("java.lang.management.RuntimeMXBean"))
+        if (classExists("java.lang.management.ManagementFactory") && classExists("java.lang.management.RuntimeMXBean"))
         {
             Class<?> managementFactory = Class.forName("java.lang.management.ManagementFactory");
             Method runtimeBeanMethod = managementFactory.getMethod("getRuntimeMXBean");
