926b6ec6da21280b8e9c7a63c7aea35f26494f2e
Ashley Donaldson <smashery@gmail.com>
2023-11-20 14:16:59 +1100

    Don't listen on IPv6 when 0.0.0.0 srvhost is requested

--- java/meterpreter/stdapi/src/main/java/com/metasploit/meterpreter/stdapi/stdapi_channel_open.java
+++ java/meterpreter/stdapi/src/main/java/com/metasploit/meterpreter/stdapi/stdapi_channel_open.java
@@ -1,20 +1,22 @@
 package com.metasploit.meterpreter.stdapi;
 
-import java.net.ConnectException;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-
 import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 import com.metasploit.meterpreter.Channel;
 import com.metasploit.meterpreter.DatagramSocketChannel;
+import com.metasploit.meterpreter.Meterpreter;
 import com.metasploit.meterpreter.ServerSocketChannel;
 import com.metasploit.meterpreter.SocketChannel;
-import com.metasploit.meterpreter.Meterpreter;
 import com.metasploit.meterpreter.TLVPacket;
 import com.metasploit.meterpreter.TLVType;
 import com.metasploit.meterpreter.command.Command;
@@ -81,18 +83,46 @@ public class stdapi_channel_open implements Command {
         return ERROR_SUCCESS;
     }
 
-    private int executeTcpServer(Meterpreter meterpreter, TLVPacket request, TLVPacket response) throws Exception {
+    private int executeTcpServer(Meterpreter meterpreter, TLVPacket request, TLVPacket response) throws UnknownHostException, IOException {
         String localHost = request.getStringValue(TLVType.TLV_TYPE_LOCAL_HOST);
         int localPort = request.getIntValue(TLVType.TLV_TYPE_LOCAL_PORT);
-        ServerSocket ss;
-        if (localHost.equals("0.0.0.0")) {
-            ss = new ServerSocket(localPort);
-        } else {
-            ss = new ServerSocket(localPort, 50, InetAddress.getByName(localHost));
-        }
+        ServerSocket ss = getSocket(localHost, localPort);
         Channel channel = new ServerSocketChannel(meterpreter, ss);
         response.add(TLVType.TLV_TYPE_CHANNEL_ID, channel.getID());
         return ERROR_SUCCESS;
+    }
+
+    private ServerSocket getSocket(String localHost, int localPort) throws UnknownHostException, IOException {
+        try {
+            if (localHost.equals("0.0.0.0")) {
+                return getIPv4Socket_java7plus(localHost, localPort);
+            }
+        } catch (UnknownHostException e) {
+            throw e;
+        }
+         catch (Exception e) {
+            // Fall back to old behaviour: will listen on IPv4 and IPv6
+        }
+        return new ServerSocket(localPort, 50, InetAddress.getByName(localHost));
+    }
+
+    // Constructing a ServerSocket directly for 0.0.0.0 will listen on both IPv4 and IPv6, which, if the operator has explicitly requested 0.0.0.0,
+    // may not be desirable. Java 7 and later support explicitly specifying IPv4 using ServerSocketChannel.open(StandardProtocolFamily.INET).
+    // To keep backwards-compatibility, we use reflection to call the newer version.
+    private ServerSocket getIPv4Socket_java7plus(String localHost, int localPort) throws Exception {
+        Class standardProtocolFamilyCls = Class.forName("java.net.StandardProtocolFamily");
+        Class protocolFamilyCls = Class.forName("java.net.ProtocolFamily");
+        java.lang.reflect.Method getValueMethod = standardProtocolFamilyCls.getMethod("valueOf", String.class);
+        Object inet = getValueMethod.invoke(null, "INET");
+        Class sscClazz = java.nio.channels.ServerSocketChannel.class;
+        java.lang.reflect.Method method = sscClazz.getMethod("open", protocolFamilyCls);
+        java.nio.channels.ServerSocketChannel server = (java.nio.channels.ServerSocketChannel)method.invoke(null, inet);
+        InetAddress addr = InetAddress.getByName(localHost);
+        InetSocketAddress sockAddr = new InetSocketAddress(addr, localPort);
+        java.lang.reflect.Method bindMethod = sscClazz.getMethod("bind", java.net.SocketAddress.class);
+        bindMethod.invoke(server, sockAddr);
+        ServerSocket ss = server.socket();
+        return ss;
     }
 
     private int executeTcpClient(Meterpreter meterpreter, TLVPacket request, TLVPacket response) throws Exception {
