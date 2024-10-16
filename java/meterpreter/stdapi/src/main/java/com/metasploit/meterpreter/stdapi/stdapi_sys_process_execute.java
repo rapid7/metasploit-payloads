f1fcfd617601fbaf6d8f820e12b2319ffde3d62d
Ashley Donaldson <smashery@gmail.com>
2024-10-16 17:29:41 +1100

    Handle discrepance of process launching on Windows between different versions of Java

--- java/meterpreter/stdapi/src/main/java/com/metasploit/meterpreter/stdapi/stdapi_sys_process_execute.java
+++ java/meterpreter/stdapi/src/main/java/com/metasploit/meterpreter/stdapi/stdapi_sys_process_execute.java
@@ -67,70 +67,83 @@ public class stdapi_sys_process_execute implements Command {
 
     // On Windows, Java quote-escapes _some_ arguments (like those with spaces), but doesn't deal correctly with some
     // edge cases; e.g. empty strings, strings that already have quotes.
-    protected String escapeArg(String arg) {
+    protected String escapeArgWindows(String arg) {
         if (arg == null) {
             return null;
         }
-        String osName = System.getProperty("os.name");
-        if (osName != null && osName.toLowerCase().contains("windows")) {
-            if (arg.equals("")) {
-                return "\"\"";
-            } else {
-                StringBuilder sb = new StringBuilder();
-                int numBackslashes = 0;
-                boolean needsQuoting = false;
-                for (int i = 0; i < arg.length(); i++) {
-                    char c = arg.charAt(i);
-                    switch (c) {
-                        case '"': {
-                            for (int nb = 0; nb < numBackslashes; nb++) {
-                                sb.append('\\');
-                            }
-                            numBackslashes = 0;
-                            sb.append('\\');
-                            break;
-                        }
-                        case '\\': {
-                            numBackslashes++;
-                            break;
-                        }
-                        case ' ':
-                        case '\t':
-                        case (char)11:
-                        {
-                            needsQuoting = true;
-                            numBackslashes = 0;
-                            break;
-                        }
-                        default: {
-                            numBackslashes = 0;
-                            break;
-                        }
-                    }
-                    sb.append(c);
-                }
-                if (needsQuoting) {
-                    for (int nb = 0; nb < numBackslashes; nb++) {
-                        sb.append('\\');
-                    }
-                    return "\"" + sb.toString() + "\"";
-                }
-                return sb.toString();
-            }
-        } else {
-            return arg;
+       if (arg.equals("")) {
+           return "\"\"";
+       } else {
+           StringBuilder sb = new StringBuilder();
+           int numBackslashes = 0;
+           boolean needsQuoting = false;
+           for (int i = 0; i < arg.length(); i++) {
+               char c = arg.charAt(i);
+               switch (c) {
+                   case '"': {
+                       for (int nb = 0; nb < numBackslashes; nb++) {
+                           sb.append('\\');
+                       }
+                       numBackslashes = 0;
+                       sb.append('\\');
+                       break;
+                   }
+                   case '\\': {
+                       numBackslashes++;
+                       break;
+                   }
+                   case ' ':
+                   case '\t':
+                   case (char)11:
+                   {
+                       needsQuoting = true;
+                       numBackslashes = 0;
+                       break;
+                   }
+                   default: {
+                       numBackslashes = 0;
+                       break;
+                   }
+               }
+               sb.append(c);
+           }
+           if (needsQuoting) {
+               for (int nb = 0; nb < numBackslashes; nb++) {
+                   sb.append('\\');
+               }
+               return "\"" + sb.toString() + "\"";
+           }
+           return sb.toString();
         }
     }
 
-    protected Process execute(String cmd, ArrayList<String> args) throws IOException {
-        ArrayList<String> cmdAndArgs = new ArrayList<String>();
-        cmdAndArgs.add(cmd);
-        for (String arg : args) {
-            cmdAndArgs.add(escapeArg(arg));
+    protected Process executeWindows(String cmd, ArrayList<String> args) throws IOException {
+        StringBuilder cmdString = new StringBuilder();
+        cmdString.append(cmd);
+        if (args.size() > 0) {
+            for (String arg : args) {
+                cmdString.append(" ");
+                cmdString.append(escapeArgWindows(arg));
+            }
         }
-        ProcessBuilder builder = new ProcessBuilder(cmdAndArgs);
-        builder.directory(Loader.getCWD());
-        return builder.start();
+
+        return execute(cmdString.toString());
+    }
+
+    protected Process execute(String cmd, ArrayList<String> args) throws IOException {
+        String osName = System.getProperty("os.name");
+        if (osName != null && osName.toLowerCase().contains("windows")) {
+            return executeWindows(cmd, args);
+        } else {
+            ArrayList<String> cmdAndArgs = new ArrayList<String>();
+            cmdAndArgs.add(cmd);
+            for (String arg : args) {
+                cmdAndArgs.add(arg);
+            }
+            ProcessBuilder builder = new ProcessBuilder(cmdAndArgs);
+            builder.directory(Loader.getCWD());
+            return builder.start();
+        }
     }
 
     protected Process execute(String cmdstr) throws IOException {
