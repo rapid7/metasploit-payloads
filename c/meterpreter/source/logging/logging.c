9a02a6e8950c8e43f3f5b380bf52cae8819b1dec
Ashley Donaldson <smashery@gmail.com>
2025-05-20 09:51:00 +1000

    Don't log to an invalid file.

--- c/meterpreter/source/logging/logging.c
+++ c/meterpreter/source/logging/logging.c
@@ -5,13 +5,18 @@ HANDLE lock = NULL;
 HANDLE hFile = NULL;
 
 HANDLE init_logging(wchar_t* filePath) {
-	hFile = CreateFileW(filePath,                // name of the write
+	HANDLE result;
+	result = CreateFileW(filePath,                // name of the write
 		GENERIC_WRITE,          // open for writing
 		FILE_SHARE_DELETE | FILE_SHARE_READ | FILE_SHARE_WRITE,    // do share (7)
 		NULL,                   // default security
 		OPEN_ALWAYS,             // create new file or open existing file
 		FILE_ATTRIBUTE_NORMAL,  // normal file
 		NULL);                  // no attr. template
+
+	if (result != INVALID_HANDLE_VALUE) {
+		hFile = result;
+	}
 	lock = CreateMutex(NULL, FALSE, NULL);
 
 	if (hFile == NULL) {
