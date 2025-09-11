ff8b0dc5d01a0ddd9593e40c895f630539305fd6
bwatters-r7 <bwatters@rapid7.com>
2025-09-11 12:35:07 -0500

    Revert "Merge pull request #744 from dledda-r7/feat/split-stdapi"
    This reverts commit c1cec39c3b9a3ed9ee9302c8af2026339c652d83, reversing
    changes made to 71053d1e16a51dee2853f180f03b139d4ca3d2c7.

--- c/meterpreter/source/extensions/stdapi/server/net/net.h
+++ c/meterpreter/source/extensions/stdapi/server/net/net.h
@@ -3,9 +3,6 @@
 
 #include <winsock2.h>
 #include <ws2tcpip.h>
-#ifdef __MINGW32__
-#include <stdint.h>
-#endif
 
 /*
  * Generic socket context
--- c/meterpreter/source/extensions/stdapi/server/precomp.h
+++ c/meterpreter/source/extensions/stdapi/server/precomp.h
@@ -9,34 +9,15 @@
 #include <iphlpapi.h>
 #include "resource/resource.h"
 
-#ifdef STDAPI_NAMESPACE_AUDIO
 #include "audio/audio.h"
-#endif
-
-#ifdef STDAPI_NAMESPACE_FS
 #include "fs/fs.h"
-#endif
-
-#ifdef STDAPI_NAMESPACE_SYS
 #include "sys/sys.h"
-#endif
-
-#ifdef STDAPI_NAMESPACE_NET
 #include "net/net.h"
-#endif
-
-#ifdef STDAPI_NAMESPACE_UI
 #include "ui/ui.h"
-#endif
-
-#ifdef STDAPI_NAMESPACE_WEBCAM
 #include "webcam/webcam.h"
 #include "webcam/audio.h"
-#endif
 
-#ifdef STDAPI_NAMESPACE_RAILGUN
 #include "railgun/railgun.h"	// PKS, win32 specific at the moment.
-#endif
 
 #include "../../../ReflectiveDLLInjection/inject/src/GetProcAddressR.h"
 #include "../../../ReflectiveDLLInjection/dll/src/ReflectiveLoader.h"
