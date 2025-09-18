c6b3963db05754f4522e23654d85e943a2ebe62e
Diego Ledda <diego_ledda@rapid7.com>
2025-09-18 16:54:05 +0200

    Revert "Revert/stdapi split"

--- c/meterpreter/source/extensions/stdapi/server/precomp.h
+++ c/meterpreter/source/extensions/stdapi/server/precomp.h
@@ -7,17 +7,37 @@
 #include "../stdapi.h"
 #include <tlhelp32.h>
 #include <iphlpapi.h>
+#include <stdint.h>
 #include "resource/resource.h"
 
+#ifdef STDAPI_NAMESPACE_AUDIO
 #include "audio/audio.h"
+#endif
+
+#ifdef STDAPI_NAMESPACE_FS
 #include "fs/fs.h"
+#endif
+
+#ifdef STDAPI_NAMESPACE_SYS
 #include "sys/sys.h"
+#endif
+
+#ifdef STDAPI_NAMESPACE_NET
 #include "net/net.h"
+#endif
+
+#ifdef STDAPI_NAMESPACE_UI
 #include "ui/ui.h"
+#endif
+
+#ifdef STDAPI_NAMESPACE_WEBCAM
 #include "webcam/webcam.h"
 #include "webcam/audio.h"
+#endif
 
+#ifdef STDAPI_NAMESPACE_RAILGUN
 #include "railgun/railgun.h"	// PKS, win32 specific at the moment.
+#endif
 
 #include "../../../ReflectiveDLLInjection/inject/src/GetProcAddressR.h"
 #include "../../../ReflectiveDLLInjection/dll/src/ReflectiveLoader.h"
