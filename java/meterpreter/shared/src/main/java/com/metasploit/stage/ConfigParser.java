68290e0af6986755f7056b9a94f8c30896245126
Spencer McIntyre <Spencer_McIntyre@rapid7.com>
2025-09-26 17:07:25 -0400

    Process the config block as a TLV packet
    This moves the Java Meterpreter inline with the new config block which
    is now a set of TLVs instead of the former static block.

--- java/meterpreter/shared/src/main/java/com/metasploit/stage/ConfigParser.java
+++ java/meterpreter/shared/src/main/java/com/metasploit/stage/ConfigParser.java
@@ -1,20 +1,13 @@
 package com.metasploit.stage;
 
-import java.io.UnsupportedEncodingException;
+import com.metasploit.TLVPacket;
+import com.metasploit.TLVType;
+
+import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class ConfigParser  {
-
-    private static final int SESSION_EXPIRY_START_LEN = 12;
-    private static final int UUID_LEN = 16;
-    private static final int GUID_LEN = 16;
-    private static final int INT_LEN = 4;
-    private static final int URL_LEN = 512;
-    private static final int UA_LEN = 256;
-    private static final int PROXY_HOST_LEN = 128;
-    private static final int PROXY_USER_LEN = 64;
-    private static final int PROXY_PASS_LEN = 64;
-    private static final int CERT_HASH_LEN = 20;
 
     private static final long MS = TimeUnit.SECONDS.toMillis(1);
 
@@ -22,111 +15,56 @@ public class ConfigParser  {
 
         Config config = new Config();
         config.rawConfig = configBytes;
-        int csr = 0;
-        config.flags = ConfigParser.unpack32(configBytes, 0);
-        csr += SESSION_EXPIRY_START_LEN;
-        config.session_expiry = MS * ConfigParser.unpack32(configBytes, csr);
-        csr += INT_LEN;
-        config.uuid = ConfigParser.readBytes(configBytes, csr, ConfigParser.UUID_LEN);
-        csr += ConfigParser.UUID_LEN;
-        config.session_guid = ConfigParser.readBytes(configBytes, csr, ConfigParser.GUID_LEN);
-        csr += ConfigParser.GUID_LEN;
-        if ((config.flags & Config.FLAG_STAGELESS) != 0) {
-            config.stageless_class = readString(configBytes, 8000, 100);
+        TLVPacket configPacket;
+
+        try {
+            configPacket = TLVPacket.fromEncoded(configBytes, null);
+            config.session_expiry = MS * configPacket.getIntValue(TLVType.TLV_TYPE_SESSION_EXPIRY);
+            config.uuid = configPacket.getRawValue(TLVType.TLV_TYPE_UUID);
+            config.session_guid = configPacket.getRawValue(TLVType.TLV_TYPE_SESSION_GUID);
+        } catch (IOException ioException) {
+            return null;
+        } catch (IllegalArgumentException illegalArgumentException) {
+            return null;
         }
 
-        while (true) {
-            if (configBytes[csr] == 0) {
-                break;
+
+        List<TLVPacket> c2Groups = configPacket.getValues(TLVType.TLV_TYPE_C2);
+        for (int i = 0; i < c2Groups.size(); ++i) {
+            TransportConfig transportConfig = new TransportConfig();
+            TLVPacket c2Group;
+            try {
+                c2Group = c2Groups.get(i);
+                transportConfig.url = c2Group.getStringValue(TLVType.TLV_TYPE_C2_URL);
+                transportConfig.comm_timeout = MS * c2Group.getIntValue(TLVType.TLV_TYPE_C2_COMM_TIMEOUT);
+                transportConfig.retry_total = MS * c2Group.getIntValue(TLVType.TLV_TYPE_C2_RETRY_TOTAL);
+                transportConfig.retry_wait = MS * c2Group.getIntValue(TLVType.TLV_TYPE_C2_RETRY_WAIT);
+                // these values are all required so if any are missing, skip adding it as a transport
+            } catch (IllegalArgumentException illegalArgumentException) {
+                continue;
             }
 
-            TransportConfig transportConfig = new TransportConfig();
-            transportConfig.url = ConfigParser.readString(configBytes, csr, URL_LEN);
-            csr += URL_LEN;
-            transportConfig.comm_timeout = MS * ConfigParser.unpack32(configBytes, csr);
-            csr += INT_LEN;
-            transportConfig.retry_total = MS * ConfigParser.unpack32(configBytes, csr);
-            csr += INT_LEN;
-            transportConfig.retry_wait = MS * ConfigParser.unpack32(configBytes, csr);
-            csr += INT_LEN;
-
             if (transportConfig.url.startsWith("http")) {
-                transportConfig.proxy = ConfigParser.readString(configBytes, csr, ConfigParser.PROXY_HOST_LEN);
-                csr += ConfigParser.PROXY_HOST_LEN;
-
-                transportConfig.proxy_user = ConfigParser.readString(configBytes, csr, ConfigParser.PROXY_USER_LEN);
-                csr += ConfigParser.PROXY_USER_LEN;
-
-                transportConfig.proxy_pass = ConfigParser.readString(configBytes, csr, ConfigParser.PROXY_PASS_LEN);
-                csr += ConfigParser.PROXY_PASS_LEN;
-
-                transportConfig.user_agent = ConfigParser.readString(configBytes, csr, ConfigParser.UA_LEN);
-                csr += ConfigParser.UA_LEN;
-
-                transportConfig.cert_hash = null;
-                byte[] loadedHash = ConfigParser.readBytes(configBytes, csr, ConfigParser.CERT_HASH_LEN);
-                csr += ConfigParser.CERT_HASH_LEN;
-
-                // we only store the cert hash value if it's got a value
-                for (int i = 0; i < loadedHash.length; i++) {
-                    if (loadedHash[i] != 0) {
+                String proxyUrl = null;
+                byte[] loadedHash;
+                try {
+                    proxyUrl = c2Group.getStringValue(TLVType.TLV_TYPE_C2_PROXY_URL);
+                } catch (IllegalArgumentException illegalArgumentException) {
+                }
+                if (proxyUrl != null) {
+                    transportConfig.proxy_url = proxyUrl;
+                    transportConfig.proxy_user = c2Group.getStringValue(TLVType.TLV_TYPE_C2_PROXY_USER, "");
+                    transportConfig.proxy_pass = c2Group.getStringValue(TLVType.TLV_TYPE_C2_PROXY_PASS, "");
+                    transportConfig.user_agent = c2Group.getStringValue(TLVType.TLV_TYPE_C2_UA, "");
+                    transportConfig.custom_headers = c2Group.getStringValue(TLVType.TLV_TYPE_C2_HEADERS, "");
+                    loadedHash = c2Group.getRawValue(TLVType.TLV_TYPE_C2_CERT_HASH, new byte[0]);
+                    if (loadedHash.length > 0) {
                         transportConfig.cert_hash = loadedHash;
-                        break;
                     }
                 }
-
-                String customHeaders = ConfigParser.readString(configBytes, csr);
-                transportConfig.custom_headers = customHeaders;
-                csr += customHeaders.length();
             }
             config.transportConfigList.add(transportConfig);
         }
         return config;
-    }
-
-    private static String readString(byte[] bytes, int offset) {
-        StringBuilder stringBuffer = new StringBuilder();
-        int byteEnd = bytes.length;
-        for (int a=offset;a<byteEnd;a++) {
-            byte byteChar = bytes[a];
-            if (byteChar == 0) {
-                break;
-            }
-            stringBuffer.append((char) (byteChar & 0xff));
-        }
-        return stringBuffer.toString();
-    }
-
-    private static String readString(byte[] bytes, int offset, int size) {
-        byte[] bytesRead = readBytes(bytes, offset, size);
-        try {
-            return new String(bytesRead, "ISO-8859-1").trim();
-        }
-        catch (UnsupportedEncodingException ex) {
-            // fallback to no encoding
-            return new String(bytesRead).trim();
-        }
-    }
-
-    private static byte[] readBytes(byte[] bytes, int offset, int size) {
-        byte[] buf = new byte[size];
-        System.arraycopy(bytes, offset, buf, 0, size);
-        return buf;
-    }
-
-    private static int unpack32(byte[] bytes, int offset) {
-        int res = 0;
-        for (int i = 0; i < 4; i++) {
-          res = res | (((int)bytes[i + offset]) & 0xFF) << (i * 8);
-        }
-        return res;
-    }
-
-    private static long unpack64(byte[] bytes, int offset) {
-        long res = 0;
-        for (int i = 0; i < 8; i++) {
-          res = res | (((long)bytes[i + offset]) & 0xFF) << (i * 8);
-        }
-        return res;
     }
 }
