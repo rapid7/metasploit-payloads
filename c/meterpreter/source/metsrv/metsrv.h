08112abf56e381f68153ccd4562dcd0c59026f52
msutovsky-r7 <martin_sutovsky@rapid7.com>
2026-04-13 12:21:05 +0200

    Fixes the arguments in metsrv.h

--- c/meterpreter/source/metsrv/metsrv.h
+++ c/meterpreter/source/metsrv/metsrv.h
@@ -57,7 +57,6 @@ VOID rand_xor_key(BYTE buffer[4]);
 
 DWORD server_setup(MetsrvConfig* config, Packet* configPacket);
 typedef DWORD (*PSRVINIT)(MetApi* api, Remote *remote);
-
 typedef DWORD (*PSRVDEINIT)(Remote *remote);
 typedef VOID (*PCMDADDED)(UINT command_id);
 typedef DWORD (*PSTAGELESSINIT)(UINT extensionId, LPBYTE data, DWORD dataSize);
